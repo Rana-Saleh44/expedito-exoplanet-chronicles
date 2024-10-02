1c87a25235517f6c40f88494f0769d0cc64192f7
Rana-Saleh44 <ranamhsaleh@gmail.com>
2024-10-02 12:56:10 +0300

    Create nasa_button.dart

---
+++ expedito_app/lib/widgets/nasa_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../models/planet.dart'; // Import the Planet model
+
+class NasaButton extends StatelessWidget {
+  final Planet planet;
+
+  NasaButton({required this.planet});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: ElevatedButton(
+        onPressed: () {
+          // Logic to open NASA website or any specific URL
+          // You might want to add the URL in the Planet model
+        },
+        style: ElevatedButton.styleFrom(
+          primary: Colors.blue, // Blue color for the button
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          padding: EdgeInsets.symmetric(vertical: 12),
+        ),
+        child: Center(
+          child: Text(
+            "View on NASA",
+            style: TextStyle(fontSize: 18, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
