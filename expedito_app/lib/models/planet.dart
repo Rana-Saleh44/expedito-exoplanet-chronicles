957b430bc5132374e1fb460b3173b8a7e7680c78
Rana-Saleh44 <ranamhsaleh@gmail.com>
2024-10-02 12:47:22 +0300

    Update planet.dart

---
+++ expedito_app/lib/models/planet.dart
@@ -0,0 +1,27 @@
+class Planet {
+  final String name;
+  final String description;
+  final String type;
+  final String distance;
+  final String star;
+  final String orbitalPeriod;
+  final String size;
+  final String temperature;
+  final String habitability;
+  final String interestingFact;
+  final String imageUrl;
+
+  Planet({
+    required this.name,
+    required this.description,
+    required this.type,
+    required this.distance,
+    required this.star,
+    required this.orbitalPeriod,
+    required this.size,
+    required this.temperature,
+    required this.habitability,
+    required this.interestingFact,
+    required this.imageUrl,
+  });
+}
