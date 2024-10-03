c2f6d3252ed17994476b3ce49d4dbffd889a3471
Rana-Saleh44 <ranamhsaleh@gmail.com>
2024-10-03 17:33:54 +0300

    updates

--- expedito_app/lib/main.dart
+++ expedito_app/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:expedito_app/screens/badges_screen.dart';
+import 'package:expedito_app/screens/no_exoplants.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: BadgesScreen(), // Set OnBoarding as the initial route
+      home: NoExoplants(), // Set OnBoarding as the initial route
     );
   }
 }
---
+++ expedito_app/lib/screens/new_player_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class NoExoplants extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Background image
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/backgrounds/Planet.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
