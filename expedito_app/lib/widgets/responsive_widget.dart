f9146072480f767d42c7b169e52e17100d830859
Rana-Saleh44 <ranamhsaleh@gmail.com>
2024-10-03 08:43:18 +0300

    Update responsive_widget.dart

---
+++ expedito_app/lib/widgets/responsive_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveWidget extends StatelessWidget {
+  final Widget Function(
+      BuildContext context, double screenWidth, double screenHeight) builder;
+
+  ResponsiveWidget({required this.builder});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    final screenWidth = screenSize.width;
+    final screenHeight = screenSize.height;
+
+    return builder(context, screenWidth, screenHeight);
+  }
+}
