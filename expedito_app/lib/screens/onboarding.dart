f945d5627cd43530ccab483170bc16448e42e763
Rana-Saleh44 <ranamhsaleh@gmail.com>
2024-10-03 08:15:09 +0300

    updates

--- expedito_app/lib/screens/onboarding.dart
+++ expedito_app/lib/screens/onboarding.dart
@@ -1,55 +1,80 @@
 import 'package:flutter/material.dart';
 
-import '../screens/signup_screen.dart'; // Import the SignUpScreen
+import '../utils/constants.dart'; // Import the constants file
 
-class onBoarding extends StatelessWidget {
+class OnBoarding extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // No need for a background color, as we are using the image as the background
       body: Stack(
         children: [
           // Background image
           Positioned.fill(
             child: Image.asset(
               'assets/images/backgrounds/onbaording.png', // Replace with your actual image path
-              fit: BoxFit.cover, // Ensure the image covers the entire screen
+              fit: BoxFit.cover,
             ),
           ),
           // Text and button overlay
           Positioned(
-            top: 50,
-            left: 50,
+            top: 89,
+            left: 57,
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
                 Text(
                   'EXPEDITO',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
+                  style: AppFonts.bold.copyWith(
+                    fontSize: 45,
+                    height: 34.95 /
+                        45, // line-height in Flutter is given as height = lineHeight / fontSize
+                    letterSpacing: -0.01,
+                    color: AppColors.white,
                   ),
+                  textAlign: TextAlign.center,
                 ),
-                SizedBox(height: 20),
+                SizedBox(height: 69), // Adjust space between texts
                 Text(
                   'Journey through the exoplanets',
-                  style: TextStyle(
-                    color: Colors.purple,
-                    fontSize: 20,
+                  style: AppFonts.regular.copyWith(
+                    fontSize: 30,
+                    height: 34.95 / 30,
+                    letterSpacing: -0.01,
+                    color: AppColors.blue,
                   ),
+                  textAlign: TextAlign.center,
                 ),
-                SizedBox(height: 50),
-                ElevatedButton.icon(
-                  // Using ElevatedButton.icon for text and icon
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SignUpScreen()),
-                    );
-                  },
-                  icon: Icon(Icons.arrow_forward), // Arrow icon
-                  label: Text('Sign Up & Play!'), // Updated button text
+                SizedBox(height: 102), // Adjust space between text and button
+                Container(
+                  width: 245,
+                  height: 82,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 81, vertical: 17),
+                  decoration: BoxDecoration(
+                    color: AppColors.white,
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Sign Up & Play!',
+                        style: AppFonts.bold.copyWith(
+                          fontSize: 21,
+                          height: 28.64 / 21,
+                          color: AppColors.black,
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                      Icon(
+                        Icons.arrow_forward,
+                        color: AppColors.black,
+                        size: 28,
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
