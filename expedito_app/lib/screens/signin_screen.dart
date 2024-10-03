c492e79542de3d4c07ffb6f26164976ffe124ee9
Rana-Saleh44 <ranamhsaleh@gmail.com>
2024-10-03 11:18:52 +0300

    updates

--- expedito_app/lib/screens/signin_screen.dart
+++ expedito_app/lib/screens/signin_screen.dart
@@ -3,109 +3,163 @@ import 'package:flutter/material.dart';
 class SignInScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Get the screen size
+    final Size screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
-      // No need for a background color, as we are using the image as the background
       body: Stack(
         children: [
           // Background image
           Positioned.fill(
             child: Image.asset(
               'assets/images/backgrounds/home.png',
-              fit: BoxFit.cover, // Ensure the image covers the entire screen
+              fit: BoxFit.cover,
             ),
           ),
-
           // Foreground content
           Padding(
-            padding: const EdgeInsets.all(16.0),
+            padding: EdgeInsets.all(screenSize.width * 0.05), // 5% padding
             child: SingleChildScrollView(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  SizedBox(height: 60),
-
+                  SizedBox(height: screenSize.height * 0.1), // 10% height
                   // Astronaut Image
                   Image.asset(
-                    'assets/images/astronauts/astronaut_logo.png', // Put your astronaut image here
-                    height: 150,
+                    'assets/images/astronauts/astronaut_logo.png',
+                    width: screenSize.width * 0.75, // 75% width
+                    height: screenSize.height * 0.4, // 40% height
+                    fit: BoxFit.cover,
                   ),
-                  SizedBox(height: 20),
-
-                  // Title "Welcome Back!"
+                  SizedBox(height: screenSize.height * 0.02), // 2% height
+                  // Title "Create an Account"
                   Text(
                     'Welcome Back!',
                     style: TextStyle(
-                      color: Colors.purple,
-                      fontSize: 28,
+                      color: Colors.blue, // Match the design color
+                      fontSize: screenSize.width * 0.07, // 7% of width
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  SizedBox(height: 20),
+                  SizedBox(height: screenSize.height * 0.02), // 2% height
 
                   // Username TextField
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'username',
-                      labelStyle: TextStyle(color: Colors.white),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.purple),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 16),
-
-                  // Password TextField
-                  TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: 'password',
-                      labelStyle: TextStyle(color: Colors.white),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.purple),
-                      ),
-                      suffixIcon: Icon(
-                        Icons.visibility_off,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 16),
-
-                  // "Or" Divider
-                  Row(
-                    children: [
-                      Expanded(child: Divider(color: Colors.white)),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                        child: Text(
-                          'Or',
-                          style: TextStyle(color: Colors.white),
+                  Container(
+                    width: screenSize.width * 0.9, // 90% of width
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'username',
+                        labelStyle: TextStyle(color: Colors.white),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue),
                         ),
                       ),
-                      Expanded(child: Divider(color: Colors.white)),
+                    ),
+                  ),
+
+                  SizedBox(height: screenSize.height * 0.02), // 2% height
+
+                  // Password TextField
+                  Container(
+                    width: screenSize.width * 0.9, // 90% of width
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'password',
+                        labelStyle: TextStyle(color: Colors.white),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue),
+                        ),
+                        suffixIcon: Icon(
+                          Icons.visibility_off,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: screenSize.height * 0.02), // 2% height
+
+                  // "Or" Text
+                  Container(
+                    width: 64,
+                    height: 26,
+                    child: Text(
+                      'Or',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Space Grotesk',
+                        fontSize: 20,
+                        fontWeight: FontWeight.normal,
+                        height: 1.75, // Line height
+                        letterSpacing: -0.01,
+                      ),
+                      textAlign: TextAlign.center, // Correctly applied here
+                    ),
+                  ),
+                  SizedBox(height: screenSize.height * 0.02), // 2% height
+
+                  // Continue with Google section
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/images/icons/devicon_google.png', // Google icon image
+                        width: 25,
+                        height: 25,
+                      ),
+                      SizedBox(width: 8), // Space between icon and text
+                      Container(
+                        width: 184,
+                        height: 35,
+                        child: Text(
+                          'Continue with Google',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Space Grotesk',
+                            fontSize: 18,
+                            fontWeight: FontWeight.normal,
+                            height: 1.75,
+                            letterSpacing: -0.01,
+                          ),
+                          textAlign: TextAlign.center, // Correctly applied here
+                        ),
+                      ),
                     ],
                   ),
-                  SizedBox(height: 16),
-
-                  // Google Sign-In Button
-                  ElevatedButton.icon(
-                    onPressed: () {
-                      // Logic for Google sign-in
-                    },
-                    icon: Image.asset(
-                      'assets/images/icons/devicon_google.png', // Put your Google logo here
-                      height: 24,
+                  SizedBox(height: screenSize.height * 0.02), // 2% height
+                  // Sign Up button
+                  Container(
+                    width: 165,
+                    height: 82,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        bottomLeft: Radius.circular(0),
+                        topRight: Radius.circular(0),
+                        bottomRight: Radius.circular(0),
+                      ),
+                      color: Colors.white,
                     ),
-                    label: Text('Continue with Google'),
-                    style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.symmetric(
-                          vertical: 12.0, horizontal: 20.0),
+                    child: TextButton(
+                      onPressed: () {
+                        // On click logic goes here
+                      },
+                      child: Text(
+                        'Log In',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Space Grotesk',
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          height: 1.5, // Line height as factor of font size
+                        ),
+                      ),
                     ),
                   ),
                 ],
--- expedito_app/lib/screens/signup_screen.dart
+++ expedito_app/lib/screens/signup_screen.dart
@@ -6,7 +6,7 @@ class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Get the screen size
-    final Size screenSize = MediaQuery.of(context).size;
+    final screenSize = MediaQuery.of(context).size; // Correctly get screen size
 
     return Scaffold(
       body: Stack(
@@ -106,88 +106,95 @@ class SignUpScreen extends StatelessWidget {
                   SizedBox(height: screenSize.height * 0.02), // 2% height
 
                   // "Or" Text
-                  Text(
-                    'Or',
-                    style: TextStyle(color: Colors.white),
+                  Container(
+                    width: 64,
+                    height: 26,
+                    child: Text(
+                      'Or',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Space Grotesk',
+                        fontSize: 20,
+                        fontWeight: FontWeight.normal,
+                        height: 1.75, // Line height
+                        letterSpacing: -0.01,
+                      ),
+                      textAlign: TextAlign.center, // Correctly applied here
+                    ),
                   ),
                   SizedBox(height: screenSize.height * 0.02), // 2% height
 
-                  // Google Sign-In Button
-                  Container(
-                    width: screenSize.width * 0.9, // 90% of width
-                    height: screenSize.height * 0.07, // Responsive height
-                    child: ElevatedButton.icon(
-                      onPressed: () {
-                        // Logic for Google sign-in
-                      },
-                      icon: Image.asset(
-                        'assets/images/icons/devicon_google.png',
-                        height: screenSize.height * 0.025, // Responsive height
+                  // Continue with Google section
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/images/icons/devicon_google.png', // Google icon image
+                        width: 25,
+                        height: 25,
                       ),
-                      label: Text(
-                        'Continue with Google',
-                        style: TextStyle(
-                          fontFamily: 'Space Grotesk',
-                          fontSize:
-                              screenSize.width * 0.045, // Responsive font size
-                          fontWeight: FontWeight.normal,
-                          color: Colors.black, // Text color for Google button
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor:
-                            Colors.white, // Set button background color
-                        padding: EdgeInsets.symmetric(
-                            vertical: 10.0), // Adjusted padding
-                        shape: RoundedRectangleBorder(
-                          borderRadius:
-                              BorderRadius.circular(8.0), // Rounded corners
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: screenSize.height * 0.04), // 4% height
-
-                  // Updated Sign Up Button
-                  Container(
-                    width: screenSize.width * 0.4, // 40% of width
-                    height: screenSize.height * 0.1, // 10% of height
-                    child: ElevatedButton(
-                      onPressed: () {
-                        // Logic for sign-up
-                      },
-                      child: Text(
-                        'Sign Up',
-                        style: TextStyle(
-                          fontFamily: 'Space Grotesk',
-                          fontSize:
-                              screenSize.width * 0.06, // Responsive font size
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black, // Text color for Sign Up button
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor:
-                            Colors.white, // Set button background color
-                        padding: EdgeInsets.symmetric(
-                            vertical: 17,
-                            horizontal: 81), // Padding as per design
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(20), // 20px for top-left
-                            topRight: Radius.circular(0), // 0px for top-right
-                            bottomLeft:
-                                Radius.circular(0), // 0px for bottom-left
-                            bottomRight:
-                                Radius.circular(0), // 0px for bottom-right
+                      SizedBox(width: 8), // Space between icon and text
+                      Container(
+                        width: 184,
+                        height: 35,
+                        child: Text(
+                          'Continue with Google',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Space Grotesk',
+                            fontSize: 18,
+                            fontWeight: FontWeight.normal,
+                            height: 1.75,
+                            letterSpacing: -0.01,
                           ),
+                          textAlign: TextAlign.center, // Correctly applied here
                         ),
-                        elevation: 0, // Remove elevation if needed
                       ),
-                    ),
+                    ],
                   ),
                   SizedBox(height: screenSize.height * 0.02), // 2% height
 
+                  // Sign Up button
+                  Container(
+                    width: screenSize.width *
+                        0.8, // Responsive width for the button
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 20), // Adjust vertical padding
+                    decoration: BoxDecoration(
+                      color: Colors.white, // Use Colors.white directly
+                      borderRadius:
+                          BorderRadius.circular(25), // Adjusted corner radius
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 10.0,
+                          offset: const Offset(0, 5), // Shadow position
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      mainAxisAlignment:
+                          MainAxisAlignment.center, // Center the text and icon
+                      children: [
+                        Text(
+                          'Sign Up',
+                          style: TextStyle(
+                            fontSize: screenSize.height *
+                                0.025, // Responsive font size
+                            color: Colors.black, // Use Colors.black directly
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(width: 10), // Space between text and icon
+                        Icon(
+                          Icons.arrow_forward,
+                          color: Colors.black, // Use Colors.black directly
+                          size: 28,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: screenSize.height * 0.02), // 2% height
                   // Already have an account? Sign in text
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
